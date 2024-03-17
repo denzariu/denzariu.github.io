5b6f33b508b5e4ee29032bcf63a7ffefdc9b7639
Denzo <40743579+denzariu@users.noreply.github.com>
2024-03-17 15:16:36 +0200

    - Added a footer.

--- src/screens/home/Why.tsx
+++ src/screens/home/Why.tsx
@@ -68,8 +68,42 @@ function Why() {
         </div>
       </div> 
 
-      <footer className="py-48">
+      <footer className="mx-auto relative">
+      
+        <svg className="fill-main-700 relative top-[0.05rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 -1.70185 18 1.702">
+          <path d="M 0 0 C 0.76 -0.43 0.506 -1.466 2.811 -1.698 C 4.692 -1.783 4.84 -0.43 6.088 -0.768 C 9.344 -2.438 9.852 -0.979 12.156 -0.705 C 13.636 -0.303 15.751 -1.952 18 0" />
+        </svg>
+        <div className="py-20 mx-auto bg-main-700">
+          <div className="flex-1 flex justify-between lg:max-w-4xl mx-auto px-6 space-x-4">
+            <div className="flex-2 sm:self-center text-main-300">
+              Created using React & Tailwind<br/>
+              by Denis @2024
+            </div>
+            <div className="flex-col text-center px-2 text-main-300 flex sm:self-center justify-between gap-1">
+              {/* <p className="self-start">Socials</p> */}
+              <div className="flex gap-2 mt-1">
+                
+                {/* LinkedIn */}
+                <a href="https://www.linkedin.com/in/denis-stefan-pinzariu/"
+                  className="fill-main-300 hover:fill-main-400"
+                >
+                  <svg className="h-8 w-8 sm:h-12 sm:w-12" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                </a>
 
+                {/* GitHub */}
+                <a href="https://www.github.com/denzariu/"
+                  className="fill-main-300 hover:fill-main-400">
+                <svg className="h-8 w-8 sm:h-12 sm:w-12" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+                </a>
+
+                {/* LinkedIn: linkedin.com/in/denis-stefan-pinzariu
+                GitHub: github.com/denzariu
+                Website: denzariu.github.io */}
+              </div>
+            </div>
+          </div>
+          
+        </div>
       </footer>
     </div>
   )
